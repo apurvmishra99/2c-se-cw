99272374b762ccddc18e2507cef97a6d0dad80c3
Lorenzo Baldini <s1853050@defago.inf.ed.ac.uk>
2019-11-28 16:15:03 +0000

    Added javadoc for Location class

--- src/uk/ac/ed/bikerental/Location.java
+++ src/uk/ac/ed/bikerental/Location.java
@@ -1,29 +1,59 @@
 package uk.ac.ed.bikerental;
 
 public class Location {
+
+    /**
+     * Location's postcode.
+    */
     private String postcode;
+
+    /**
+     * Location's address.
+    */
     private String address;
-    
+
+
+    /**
+     * Location constructor.
+     *
+     * @param  postcode Postcode
+     * @param  address  Address
+     * @return          Location object.
+     */
     public Location(String postcode, String address) {
         assert postcode.length() >= 6;
         this.postcode = postcode;
         this.address = address;
     }
-    
-    public boolean isNearTo(Location other) {
-        if (this.postcode.substring(0, 3).equals(other.postcode.substring(0, 3))) {
+
+    /**
+     * Location constructor.
+     *
+     * @param otherLocation Location object to check vicinity with
+     * @return              True if the locations are near each other.
+     */
+    public boolean isNearTo(Location otherLocation) {
+        if (this.postcode.substring(0, 2).equals(otherLocation.postcode.substring(0, 2))) {
             return true;
         }
         return false;
     }
 
+    /**
+     * Postcode getter.
+     *
+     * @return Object's postcode.
+     */
     public String getPostcode() {
         return postcode;
     }
 
+    /**
+     * Address getter.
+     *
+     * @return Object's address.
+     */
     public String getAddress() {
         return address;
     }
-    
-    // You can add your own methods here
 }
