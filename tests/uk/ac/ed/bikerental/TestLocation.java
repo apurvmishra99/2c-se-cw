12585fda1c046a28002266a64acbffc51b500267
Lorenzo Baldini <s1853050@defago.inf.ed.ac.uk>
2019-11-28 16:32:20 +0000

    Added jUnit for Location

--- tests/uk/ac/ed/bikerental/TestLocation.java
+++ tests/uk/ac/ed/bikerental/TestLocation.java
@@ -4,10 +4,43 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 class TestLocation {
+
+    Location loc1, loc2, loc3, loc4;
+
     @BeforeEach
     void setUp() throws Exception {
-        // TODO: setup some resources before each test
+        loc1 = new Location("EH7 4EG", "38 Haddington Place");
+        loc2 = new Location("EH3 9LW", "15 Leven Terrace");
+        loc3 = new Location("CA3 1LW", "Cagliariiii");
+        loc4 = new Location("eh1 1jh", "Somewhere in Edi");
     }
-    
-    // TODO: put some tests here
+
+    // Check loc1 is near loc2 (expect True as both 'EH')
+    @Test
+    void isNearTo_true() {
+        boolean loc1loc2 = loc1.isNearTo(loc2);
+        assertTrue(loc1loc2);
+    }
+
+    // Check loc1 is near loc2 (expect True as both 'EH') (expect case insensitive)
+    @Test
+    void isNearTo_symmetric() {
+        boolean loc1loc2 = loc1.isNearTo(loc2);
+        boolean loc2loc1 = loc2.isNearTo(loc1);
+        assertEquals(loc1loc2, loc2loc1);
+    }
+
+    // Check loc1 is near loc2 (expect True as both 'EH') (expect case insensitive)
+    @Test
+    void isNearTo_caseInsensitive() {
+        boolean loc1loc4 = loc1.isNearTo(loc4);
+        assertTrue(loc1loc4);
+    }
+
+    // Test loc1 is near loc3 (expect False)
+    @Test
+    void isNearTo_false() {
+        boolean loc1loc3 = loc1.isNearTo(loc3);
+        assertFalse(loc1loc3);
+    }
 }
